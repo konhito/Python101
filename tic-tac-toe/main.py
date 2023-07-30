df7115f4526fe95e625667684715ff37063a8ce7
konhito <konhito2@gmail.com>
2023-07-30 22:14:28 +0530

    make some changes in main.py

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,43 +1,49 @@
 import random
 
-choices = ["rock", "paper", "scissors"]
+while True:
+    choices = ["rock", "paper", "scissors"]
 
-computer = random.choice(choices)
-player = None
+    computer = random.choice(choices)
+    player = None
 
-while player not in choices:
-    player = input("rock , paper or scissors?: ").lower()
+    while player not in choices:
+        player = input("rock , paper or scissors?: ").lower()
 
+    if player == computer:
+        print(f"Computer: {computer}")
+        print(f"player: {player}")
+        print("IT's a tie ")
 
-if player == computer:
-    print(f"Computer: {computer}")
-    print(f"player: {player}")
-    print("IT's a tie ")
+    elif player == "rock":
+        if computer == "paper":
+            print(f"Computer: {computer}")
+            print(f"player: {player}")
+            print("You loose! ")
+        if computer == "scissors":
+            print(f"Computer: {computer}")
+            print(f"player: {player}")
+            print("You win! ")
+    elif player == "paper":
+        if computer == "scissors":
+            print(f"Computer: {computer}")
+            print(f"player: {player}")
+            print("You loose! ")
+        if computer == "rock":
+            print(f"Computer: {computer}")
+            print(f"player: {player}")
+            print("You win! ")
+    elif player == "scissors":
+        if computer == "rock":
+            print(f"Computer: {computer}")
+            print(f"player: {player}")
+            print("You loose! ")
+        if computer == "paper":
+            print(f"Computer: {computer}")
+            print(f"player: {player}")
+            print("You win! ")
 
-elif player == "rock":
-    if computer == "paper":
-        print(f"Computer: {computer}")
-        print(f"player: {player}")
-        print("You loose! ")
-    if computer == "scissors":
-        print(f"Computer: {computer}")
-        print(f"player: {player}")
-        print("You win! ")
-elif player == "paper":
-    if computer == "scissors":
-        print(f"Computer: {computer}")
-        print(f"player: {player}")
-        print("You loose! ")
-    if computer == "rock":
-        print(f"Computer: {computer}")
-        print(f"player: {player}")
-        print("You win! ")
-elif player == "scissors":
-    if computer == "rock":
-        print(f"Computer: {computer}")
-        print(f"player: {player}")
-        print("You loose! ")
-    if computer == "paper":
-        print(f"Computer: {computer}")
-        print(f"player: {player}")
-        print("You win! ")
+    play_again = input("plaay again? (yes/no): ").lower()
+
+    if play_again != "yes":
+        break
+print("BYE! amogos")
